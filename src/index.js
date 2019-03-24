dd499a69a2efd680326a96cfcb04ab19309cb170
Dmitry <davidowski7777@gmail.com>
2019-03-24 18:55:35 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var opens = '';
+  var closes = '';
+    bracketsConfig.forEach( (cfg) => {
+        opens += cfg[0];
+        closes += cfg[1];
+    });
+
+    var stack = [];
+
+    for (var i in str) {
+        var s = str[i];
+        opn = !!~opens.indexOf(s);
+        cls = !!~closes.indexOf(s);
+        if (opn && cls) { 
+            if (s === stack[stack.length-1]) {
+                stack.pop(s);
+            } else {
+                stack.push(s);
+            }
+        } else {
+            if (opn) {
+                stack.push(s);
+            } else {
+                if (opens.indexOf(stack.pop()) !== closes.indexOf(s)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+};
